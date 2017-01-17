ca5284fd497d1209562e53ef5e6deaa809612dda
Saptarsi Chowdhury <saptarsi.chowdhury@gmail.com>
2017-01-18 01:29:17 +0530

    Update script.js

--- Module5/Assignment_Solution/js/script.js
+++ Module5/Assignment_Solution/js/script.js
@@ -1,12 +1,4 @@
-/*
- * Course_name: HTML, CSS, and Javascript for Web Developers
- * Section: Coursera @ Johns Hopkins University
- * Name: MWANIA Joseph Musembi
- * Date: August 6, 2016
- * Assignment: Module 4 Coding Assignment
- */
- 
-$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
   $("#navbarToggle").blur(function (event) {
@@ -23,11 +15,11 @@ var dc = {};
 
 var homeHtmlUrl = "snippets/home-snippet.html";
 var allCategoriesUrl = 
-  "http://davids-restaurant.herokuapp.com/categories.json";
+  "https://davids-restaurant.herokuapp.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl = 
-  "http://davids-restaurant.herokuapp.com/menu_items.json?category=";
+  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
@@ -353,4 +345,3 @@ function insertItemPortionName(html,
 global.$dc = dc;
 
 })(window);
-
